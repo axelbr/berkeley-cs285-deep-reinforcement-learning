bb5db37dcafccf931775992f729c1cd17786c8f1
r <axel.brunnbauer@gmx.at>
2021-12-13 14:57:38 +0100

    add figures for q1 part 1

--- hw5/report/scripts/rnd_vs_random.sh
+++ hw5/report/scripts/rnd_vs_random.sh
@@ -3,7 +3,7 @@
 env_1=PointmassEasy-v0
 env_2=PointmassHard-v0
 
-python cs285/scripts/run_hw5_expl.py --env_name $env_1 --num_timesteps 10000 --use_rnd --unsupervised_exploration --exp_name q1_${env_2}_rnd & \
-python cs285/scripts/run_hw5_expl.py --env_name $env_1 --num_timesteps 10000 --unsupervised_exploration --exp_name q1_${env_1}_random & \
-python cs285/scripts/run_hw5_expl.py --env_name $env_2 --num_timesteps 10000 --use_rnd --unsupervised_exploration --exp_name q1_${env_2}_rnd & \
-python cs285/scripts/run_hw5_expl.py --env_name $env_2 --num_timesteps 10000 --unsupervised_exploration --exp_name q1_${env_2}_random
+python cs285/scripts/run_hw5_expl.py --env_name $env_1 --num_timesteps 10000 --use_rnd --unsupervised_exploration --exp_name q1_rnd & \
+python cs285/scripts/run_hw5_expl.py --env_name $env_1 --num_timesteps 10000 --unsupervised_exploration --exp_name q1_random & \
+python cs285/scripts/run_hw5_expl.py --env_name $env_2 --num_timesteps 10000 --use_rnd --unsupervised_exploration --exp_name q1_rnd & \
+python cs285/scripts/run_hw5_expl.py --env_name $env_2 --num_timesteps 10000 --unsupervised_exploration --exp_name q1_random
